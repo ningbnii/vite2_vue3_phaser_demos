a427b074ccf96973d6ec032f475ee4d0c1076e37
ning.bnii@163.com <>
2022-02-17 17:26:46 +0800

    update

--- src/components/game-objects/container.js
+++ src/components/game-objects/container.js
@@ -1,22 +1,107 @@
-class Example extends Phaser.Scene {
+class SceneA extends Phaser.Scene {
   constructor() {
-    super()
-    this.iter = 0
+    super({
+      key: 'SceneA',
+      active: true,
+    })
+    // super('SceneA')
+    this.stars = []
+    this.cameraRotation = 0
   }
 
   preload() {
-    this.load.image('mushroom', 'assets/sprites/mushroom2.png')
+    this.load.image('metal', 'assets/textures/alien-metal.jpg')
+    this.load.atlas('megaset', 'assets/atlas/megaset-0.png', 'assets/atlas/megaset-0.json')
   }
 
   create() {
-    var image1 = this.add.image(0, -30, 'mushroom')
-    var image2 = this.add.image(-40, 30, 'mushroom')
-    var image3 = this.add.image(40, 30, 'mushroom')
-    this.container = this.add.container(400, 200, [image1, image2, image3]).setSize()
-    this.container.setSize(128, 64)
-    this.physics.world.enable(this.container)
-    this.container.body.setVelocity(100, 200).setBounce(1, 1).setCollideWorldBounds(true)
+    var colorsTable = [0xff0000, 0x00ff00, 0x0000ff, 0xff00ff, 0xffff00, 0x00ffff]
+    for (var i = 0; i < 500; ++i) {
+      let starGraphics = this.add.graphics({ x: Math.random() * 800, y: Math.random() * 600 })
+      this.drawStar(starGraphics, 0, 0, 5, 100, 50, colorsTable[Math.floor(Math.random() * 6)], 0xff0000)
+      starGraphics.fillRect(100, 100, 100, 100)
+      starGraphics.rotation = Math.random()
+      starGraphics.scaleX = 0.1 + Math.abs(Math.sin(starGraphics.rotation)) * 0.2
+      starGraphics.scaleY = 0.1 + Math.abs(Math.sin(starGraphics.rotation)) * 0.2
+      this.stars.push(starGraphics)
+    }
+  }
+  update() {
+    for (var i = 0; i < this.stars.length; ++i) {
+      var star = this.stars[i]
+      star.rotation += 0.01
+      star.scaleX = 0.1 + Math.abs(Math.sin(star.rotation)) * 0.2
+      star.scaleY = 0.1 + Math.abs(Math.sin(star.rotation)) * 0.2
+    }
+  }
+
+  drawStar(graphics, cx, cy, spikes, outerRadius, innerRadius, color, lineColor) {
+    var rot = (Math.PI / 2) * 3 // 初始弧度
+    var x = cx
+    var y = cy
+    var step = Math.PI / spikes // 每个角之间的弧度间隔
+    graphics.lineStyle(10, lineColor, 1)
+    graphics.fillStyle(color, 1)
+    graphics.beginPath()
+    graphics.moveTo(cx, cy - outerRadius)
+    for (let i = 0; i < spikes; i++) {
+      x = cx + Math.cos(rot) * outerRadius
+      y = cy + Math.sin(rot) * outerRadius
+      graphics.lineTo(x, y)
+      rot += step
+
+      x = cx + Math.cos(rot) * innerRadius
+      y = cy + Math.sin(rot) * innerRadius
+      rot += step
+    }
+
+    graphics.lineTo(cx, cy - outerRadius)
+    graphics.closePath()
+    graphics.fillPath()
+    graphics.strokePath()
   }
 }
 
-export default Example
+class SceneB extends Phaser.Scene {
+  constructor() {
+    super({ key: 'SceneB', active: false })
+  }
+
+  create() {
+    // this.cameras.main.setBackgroundColor(0xff0000);
+    this.cameras.main.setViewport(100, 100, 600, 400)
+
+    var graphics = this.add.graphics()
+
+    graphics.fillStyle(0x00ff00, 1)
+
+    graphics.beginPath()
+
+    graphics.moveTo(400, 100)
+    graphics.lineTo(200, 278)
+    graphics.lineTo(340, 430)
+    graphics.lineTo(650, 300)
+    graphics.lineTo(700, 180)
+    graphics.lineTo(600, 80)
+
+    graphics.closePath()
+    graphics.fillPath()
+  }
+}
+
+class SceneC extends Phaser.Scene {
+  constructor() {
+    super('SceneC')
+  }
+
+  create() {
+    this.add.image(400, 300, 'megaset', 'cactuar')
+
+    const graphics = this.add.graphics()
+
+    graphics.fillStyle(0xffff00, 0.8)
+    graphics.fillTriangle(400, 400, 690, 50, 780, 300)
+  }
+}
+
+export { SceneA, SceneC, SceneB }
--- src/views/game-objects/container.vue
+++ src/views/game-objects/container.vue
@@ -9,12 +9,12 @@ cont
 import { ref, onMounted, onUnmounted, watch } from 'vue'
 import * as Phaser from 'Phaser'
 
-import Example from '../../components/game-objects/container'
+import { SceneA, SceneC, SceneB } from '../../components/game-objects/container'
 
 let myCanvas = ref(null)
 let canvasBox = ref(null)
 let game
-let exampleScene
+let sceneA, sceneC, sceneB
 let text = ref('')
 
 onMounted(() => {
@@ -28,12 +28,17 @@ onMounted(() => {
     this.create(scene)
   }
 
-  exampleScene = new Example()
+  sceneA = new SceneA()
+  sceneC = new SceneC()
+  sceneB = new SceneB()
+
+  // console.log(exampleScene.sprite)
+  // overlayScene = new Overlay()
 
   let config = {
     type: Phaser.AUTO,
     scale: {
-      mode: Phaser.DOM.FIT, // 自适应
+      // mode: Phaser.DOM.FIT, // 自适应
       parent: myCanvas.value,
       autoCenter: Phaser.DOM.CENTER_BOTH, // 自动居中
       width: 800,
@@ -53,7 +58,10 @@ onMounted(() => {
     banner: false,
     backgroundColor: '#2d2d2d',
     // transparent: true, // 透明，backgroundColor失效
-    scene: [exampleScene],
+    scene: {
+      create: create,
+      update: update,
+    },
     pixelArt: true, //将 antialias 设置为 false 并将 roundPixels 设置为 true。 这是像素艺术游戏的最佳设置
     roundPixels: true,
     audio: {
@@ -62,11 +70,88 @@ onMounted(() => {
       // noAudio: false, // if this is true, it works, with audio enabled it fails
     },
     physics: {
-      default: 'arcade',
+      default: 'matter',
+      matter: {
+        debug: true,
+        gravity: {
+          y: 0.3,
+        },
+      },
     },
   }
+  var Point = function (x, y, time) {
+    this.x = x
+    this.y = y
+    this.time = time
+  }
+
+  var normalizeValue = function (value, min, max) {
+    return (value - min) / (max - min)
+  }
+  var linearInterpolation = function (norm, min, max) {
+    return (max - min) * norm + min
+  }
+
+  var trail
+  var points = []
+  var head = { x: 0, y: 0 }
 
   game = new Phaser.Game(config)
+
+  function create() {
+    trail = this.add.graphics()
+    this.input.on('pointermove', (pointer) => {
+      head.x = pointer.x
+      head.y = pointer.y
+      points.push(new Point(head.x, head.y, 4))
+    })
+  }
+
+  function update() {
+    trail.clear()
+
+    // 为什么要大于4个点
+    // 因为要收尾，收尾效果用到了4个点
+    if (points.length > 4) {
+      trail.lineStyle(1, 0xffff00, 1)
+      trail.beginPath()
+      trail.lineStyle(0, 0xffff00, 1)
+      // 移动到第一个点的位置
+      trail.moveTo(points[0].x, points[0].y)
+
+      // 从第二个点开始，一直到倒数第4个点停止
+      for (let index = 1; index < points.length - 4; ++index) {
+        var point = points[index]
+
+        // 计算线条宽度，通过一个线性差值算法
+        // 透明度0.5
+        trail.lineStyle(linearInterpolation(index / (points.length - 4), 0, 20), ((0xff & 0x0ff) << 16) | ((((linearInterpolation(index / points.length, 0x00, 0xff) | 0) & 0x0ff) << 8) | (0x00 & 0x0ff)), 0.5)
+        trail.lineTo(point.x, point.y)
+      }
+      var count = 0
+      // 剩下的4个点，收尾轨迹
+      for (let index = points.length - 4; index < points.length; ++index) {
+        var point = points[index]
+        // console.log(linearInterpolation(count++ / 4, 20, 0))
+        trail.lineStyle(linearInterpolation(count++ / 4, 20, 0), ((0xff & 0x0ff) << 16) | ((((linearInterpolation(index / points.length, 0x00, 0xff) | 0) & 0x0ff) << 8) | (0x00 & 0xff)), 1.0)
+        trail.lineTo(point.x, point.y)
+      }
+
+      trail.strokePath()
+      trail.closePath()
+    }
+    for (var index = 0; index < points.length; ++index) {
+      var point = points[index]
+
+      // 控制线显示的长度，这个值越小，显示的长度会越长
+      point.time -= 0.5
+      if (point.time <= 0) {
+        // 移除该点
+        points.splice(index, 1)
+        index -= 1
+      }
+    }
+  }
 })
 
 onUnmounted(() => {
